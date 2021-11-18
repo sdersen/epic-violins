b68e3ca25cec14051472659374e35c9738063330
patrosk <naiade99@gmail.com>
2021-11-18 10:31:50 +0100

    fixed card script

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const readMoreBtns = document.querySelectorAll('.read-more-btn');
-const cards = document.querySelectorAll('.card');
-const productsBox = document.querySelectorAll('.product-box');
-cards.forEach(())
+const productsBoxes = document.querySelectorAll(".product-box");
 
-readMoreBtns.forEach((readMoreBtn) => {
+productsBoxes.forEach((productBox) => {
+  const button = productBox.querySelector(".read-more-btn");
+  const card = productBox.querySelector(".card");
+  const close = productBox.querySelector(".close");
+  button.addEventListener("click", () => {
+    card.classList.add("show-card");
+    console.log("hej");
+  });
+  close.addEventListener("click", () => {
+    card.classList.remove("show-card");
+  });
+  console.log(productBox);
+  console.log(button);
+});
 
-    readMoreBtn.addEventListener('click', () => {
-        
-    })
-})
+console.log(productsBoxes);
