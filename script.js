1bceb9d89a859ca19d58ffb22f05f91d558e7afa
Sofia Dersén <Sofia_dersen@hotmail.com>
2021-11-18 10:02:46 +0100

    Added first card on products

---
+++ script.js
@@ -0,0 +1,11 @@
+const readMoreBtns = document.querySelectorAll('.read-more-btn');
+const cards = document.querySelectorAll('.card');
+const productsBox = document.querySelectorAll('.product-box');
+cards.forEach(())
+
+readMoreBtns.forEach((readMoreBtn) => {
+
+    readMoreBtn.addEventListener('click', () => {
+        
+    })
+})
